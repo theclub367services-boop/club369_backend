3550e7122ed4ebe699048b855c934ba74244129e
Ajanthan-SV <harissanthisurya@gmail.com>
2026-02-16 22:22:56 +0530

    frontend config

--- cloud369_backend/settings.py
+++ cloud369_backend/settings.py
@@ -211,13 +211,13 @@ CORS_ALLOWED_ORIGINS = [
     # "http://localhost:5173",
     # "http://127.0.0.1:3000",
     # "http://127.0.0.1:5173",
-    "https://club369-frontend.pages.dev/", # Replace with your Cloudflare Pages URL
+    "https://club369-frontend.pages.dev", # Replace with your Cloudflare Pages URL
 ]
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = [
     # "http://localhost:3000",
     # "http://localhost:5173",
-    "https://club369-frontend.pages.dev/", # Replace with your Cloudflare Pages URL
+    "https://club369-frontend.pages.dev", # Replace with your Cloudflare Pages URL
 ]
 
