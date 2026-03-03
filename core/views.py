b12b5bbdf01d0e8da4aa6da3a0f8c571b8f76cbe
Ajanthan-SV <harissanthisurya@gmail.com>
2026-03-03 16:39:22 +0530

    error: APIview import fix

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import login, logout
 from rest_framework import status, generics, permissions, views
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.utils import timezone
